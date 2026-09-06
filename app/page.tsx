2f61492a838178e3bd242837b73d7b9faadbfe60
Harshi <harshitashinge246@nhitm.ac.in>
2026-09-06 19:13:56 +0530

    Fix CI build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
-const ciFailure: number = "this should fail";
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
